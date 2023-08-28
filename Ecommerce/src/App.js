14205dcb7d416d1fef59074970c2e517ae29105b
VishuPathania <vishalpathania0003@gmail.com>
2023-08-28 20:51:45 +0530

    make navigation and post request in firebase

---
+++ Ecommerce/src/App.js
@@ -0,0 +1,33 @@
+import { useState} from "react";
+import Header from "./components/Header";
+import CartList from "./components/CartList";
+import Home from "./components/pages/Home";
+import Store from "./components/pages/Store";
+import About from "./components/pages/About";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ContactUs from "./components/pages/ContactUs";
+
+const App = () => {
+  const [showCart, setShowCart] = useState(false);
+
+  const handleShow = (value) => {
+    setShowCart(value);
+  };
+
+  return (
+    <div>
+      <Header handleShow={handleShow} />
+      
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="store" element={showCart ? <CartList /> : <Store />} />
+        <Route path="about" element={<About />} />
+        <Route path ='ContactUs' element={<ContactUs/>}/>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ Ecommerce/src/components/CartItem.js
@@ -0,0 +1,70 @@
+export const CartitemList = [
+    {
+      id: 1,
+      title: "Album 1",
+  
+      price: 100,
+  
+      imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+  
+      quantity: 2,
+    },
+  
+    {
+      id: 2,
+  
+      title: "Album 2",
+  
+      price: 50,
+  
+      imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+  
+      quantity: 3,
+    },
+  
+    {
+      id: 3,
+  
+      title: "Album 3",
+  
+      price: 70,
+  
+      imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+  
+      quantity: 1,
+    },
+  ];
+  
+  export const productsArr = [
+    {
+      title: "Album 1",
+  
+      price: 100,
+  
+      imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    },
+  
+    {
+      title: "Album 2",
+  
+      price: 50,
+  
+      imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    },
+  
+    {
+      title: "Album 3",
+  
+      price: 70,
+  
+      imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    },
+  
+    {
+      title:"Album 4",
+  
+      price: 100,
+  
+      imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+    },
+  ];
